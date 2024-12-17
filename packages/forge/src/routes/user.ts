851d4d6fb7b2ae11de4bfb7cdf27cc11545077d1
adamdottv <2363879+adamdottv@users.noreply.github.com>
2024-12-17 14:45:22 -0600

    feat: domains in forge

--- packages/forge/src/routes/user.ts
+++ packages/forge/src/routes/user.ts
@@ -1,13 +1,23 @@
 import { Layout, Page, io } from "@forgeapp/sdk";
+import { count, desc, sql } from "@terminal/core/drizzle/index";
 import { useTransaction } from "@terminal/core/drizzle/transaction";
 import { userTable } from "@terminal/core/user/user.sql";
 
 export const User = new Page({
   name: "User",
   handler: async (c) => {
+    const domains = await useTransaction(async (tx) => ({
+      data: await tx
+        .select({ domain: sql`substring_index(${userTable.email}, '@', -1)` })
+        .from(userTable)
+        .groupBy(sql`substring_index(${userTable.email}, '@', -1)`)
+        .orderBy(desc(count(userTable.id)))
+        .then((r) => r.map((d) => d.domain)),
+    }));
     return new Layout({
       title: "User",
       children: [
+        io.display.object("Domains", domains),
         io.display.table("", {
           getData: async (input) => {
             return useTransaction(async (tx) => ({
