72f333669eceaea682f966bc378304913558c0b2
Dax Raad <d@ironbay.co>
2024-12-18 10:30:13 -0500

    sync

--- packages/forge/src/routes/order.ts
+++ packages/forge/src/routes/order.ts
@@ -212,7 +212,7 @@ export const Order = new Page({
             const bytes = await mergedPdf.save();
             console.log("saved pdf");
             const key = `labels/${new Date().toISOString()}-${count}.pdf`;
-            console.log("sending to s3", key);
+            console.log("sending to s3", key, "length", bytes.length);
             await s3
               .send(
                 new PutObjectCommand({
