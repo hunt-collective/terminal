7aac852811a0bcbb215439080cd0707869f53ae3
adamdottv <2363879+adamdottv@users.noreply.github.com>
2024-12-30 06:31:27 -0600

    wip: docs

--- packages/www/src/components/markdown/img.tsx
+++ packages/www/src/components/markdown/img.tsx
@@ -1,17 +1,22 @@
 import { type Component, type ParentProps } from 'solid-js'
 import EmptyLine from './empty-line'
+import Line from './line'
 
 const Img: Component<{ alt?: string } & ParentProps> = (props) => {
   return (
     <>
       <EmptyLine />
-      <figure class="flex flex-col items-center bg-gray-1 justify-center border-y">
-        <img {...props} class="">
-          {props.children}
-        </img>
+      <figure class="">
+        <div class="bg-gray-1 border-y">
+          <img {...props} class="mx-auto">
+            {props.children}
+          </img>
+        </div>
         {props.alt && (
-          <figcaption class="text-gray-10 italic lowercase">
-            {props.alt}
+          <figcaption class="text-gray-10 italic lowercase text-center">
+            <Line>
+              <span class="w-full text-center">{props.alt}</span>
+            </Line>
           </figcaption>
         )}
       </figure>
