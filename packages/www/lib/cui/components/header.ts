2b022f53b8d169eb721c8bb7a7b25f1324c12b22
adamdottv <2363879+adamdottv@users.noreply.github.com>
2025-01-03 14:26:09 -0600

    wip: console shop

--- packages/www/lib/cui/components/header.ts
+++ packages/www/lib/cui/components/header.ts
@@ -1,68 +1,65 @@
 import { styles } from '../render'
-import { Box, Flex, Text, Stack, Break } from './'
+import { Box, Flex, Text } from './'
 import type { Component } from '../render'
 
 export function Header(): Component {
   return (model, parentContext) => {
-    return Stack([
-      Box(
-        Flex(
-          [
-            Flex(
-              [
-                Text('terminal', { style: styles.white }),
-                Flex(
-                  [
-                    Text('s', { style: styles.white }),
-                    Text('shop', {
-                      style: {
-                        color: model.view === 'shop' ? 'white' : 'gray',
-                      },
-                    }),
-                  ],
-                  { gap: 1 },
-                ),
-                Flex(
-                  [
-                    Text('a', { style: styles.white }),
-                    Text('account', {
-                      style: {
-                        color: model.view === 'account' ? 'white' : 'gray',
-                      },
-                    }),
-                  ],
-                  { gap: 1 },
-                ),
-              ],
-              { gap: 5 },
-            ),
-            Flex(
-              [
-                Text('c', { style: styles.white }),
-                Text('cart', {
-                  style: {
-                    color: model.view === 'cart' ? 'white' : 'gray',
-                  },
-                }),
-                Text(`$ ${(model.cart?.subtotal ?? 0) / 100}`, {
-                  style: styles.white,
-                }),
-                Text(
-                  `[${model.cart?.items.reduce((acc, item) => acc + item.quantity, 0) ?? 0}]`,
-                  { style: { color: 'gray' } },
-                ),
-              ],
-              { gap: 1 },
-            ),
-          ],
-          { justify: 'between', gap: 1 },
-        ),
-        {
-          padding: { x: 2 },
-          style: { background: '#1e1e1e', padding: '7px 0px' },
-        },
+    return Box(
+      Flex(
+        [
+          Flex(
+            [
+              Text('terminal', { style: styles.white }),
+              Flex(
+                [
+                  Text('s', { style: styles.white }),
+                  Text('shop', {
+                    style: {
+                      color: model.view === 'shop' ? 'white' : 'gray',
+                    },
+                  }),
+                ],
+                { gap: 1 },
+              ),
+              Flex(
+                [
+                  Text('a', { style: styles.white }),
+                  Text('account', {
+                    style: {
+                      color: model.view === 'account' ? 'white' : 'gray',
+                    },
+                  }),
+                ],
+                { gap: 1 },
+              ),
+            ],
+            { gap: 5 },
+          ),
+          Flex(
+            [
+              Text('c', { style: styles.white }),
+              Text('cart', {
+                style: {
+                  color: model.view === 'cart' ? 'white' : 'gray',
+                },
+              }),
+              Text(`$ ${(model.cart?.subtotal ?? 0) / 100}`, {
+                style: styles.white,
+              }),
+              Text(
+                `[${model.cart?.items.reduce((acc, item) => acc + item.quantity, 0) ?? 0}]`,
+                { style: { color: 'gray' } },
+              ),
+            ],
+            { gap: 1 },
+          ),
+        ],
+        { justify: 'between', gap: 1 },
       ),
-      Break(), // Empty line after header
-    ])(model, parentContext)
+      {
+        padding: { x: 2 },
+        style: { background: '#1e1e1e', padding: '7px 0px' },
+      },
+    )(model, parentContext)
   }
 }
