9a4f050dc7f190e42f6379bde6eaf22cd9c606bd
adamdottv <2363879+adamdottv@users.noreply.github.com>
2025-01-24 14:16:57 -0600

    chore: cleanup app

--- packages/textjs/src/components/app.tsx
+++ packages/textjs/src/components/app.tsx
@@ -1,33 +1,12 @@
-import React, { PureComponent, type ReactNode } from "react"
+import { PropsWithChildren } from "react"
 import { createKeyboardManager, KeyboardContext } from "../keyboard"
-
-type Props = {
-  readonly children: ReactNode
-}
-type State = {}
 
 const keyboardManager = createKeyboardManager()
 
-export default class App extends PureComponent<Props, State> {
-  static displayName = "TextjsApp"
-
-  static getDerivedStateFromError(error: Error) {
-    return { error }
-  }
-
-  override state = {}
-
-  override render() {
-    return (
-      <KeyboardContext.Provider value={keyboardManager}>
-        {this.props.children}
-      </KeyboardContext.Provider>
-    )
-  }
-
-  override componentDidMount() {}
-
-  override componentWillUnmount() {}
-
-  override componentDidCatch(error: Error) {}
+export default function App(props: PropsWithChildren) {
+  return (
+    <KeyboardContext.Provider value={keyboardManager}>
+      {props.children}
+    </KeyboardContext.Provider>
+  )
 }
